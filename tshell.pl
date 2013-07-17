f07d71cb2e4370f5a8d28460d1925cf73b93e4cb
Ryan Petty <nyrrules@gmail.com>
2013-07-16 23:54:28 -0700

    T2: Did more for the reply feature so it can support multiple different timeline style feeds (mentions, timeline) It sucks though

--- tshell.pl
+++ tshell.pl
@@ -54,5 +54,47 @@ sub delegate {
 		}
 		my $tweet 		= join (' ', @cont_arr);
 		$twitter->reply( $reply_id, 'timeline', $include_others, $tweet );
+	} elsif ($command eq 'test'){
+		_handle_reply(\@cont_arr);
 	}
 }
+
+sub _handle_reply {
+	my $content_arr = shift || (print "Empty command!\n" and return);
+	
+	# Initialize options and create defaults
+	my %options;
+	$options{source} 	= 'timeline';
+	$options{all} 		= 0;
+
+	my $index = 0;
+	my $option_index = 0;
+	my $end = scalar @{$content_arr};
+	while ( $index < $end and $index == $option_index ){
+		my $current = $$content_arr[$index];
+		if ( $current eq '-s' or $current eq '--source' ) {
+			my $value = $$content_arr[$index+1];
+			if ( $value eq 'm' or $value eq 'mentions' ){
+				$options{source} = 'mentions';
+				$option_index += 2;
+			} elsif ( $value eq 't' or $value eq 'timeline' ) {
+				$options{source} = 'timeline';
+				$option_index += 2
+			}
+			$index++;
+		} elsif ( $current eq '-a' or $current eq '--all' ) {
+			$options{all} = 1;
+			$option_index++;
+		}
+		$index++;
+	}
+	
+	foreach my $index (1..$option_index){
+		shift @$content_arr;
+	}
+	my $tweet = join( ' ', @$content_arr );
+	print $tweet;
+	
+}
+
+1;
